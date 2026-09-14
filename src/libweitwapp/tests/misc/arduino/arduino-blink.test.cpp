40b1865c921ab8ba1a2f6134f3022243d45c9401
Tobias <tobias.weiberlenn@mailbox.org>
2026-09-14 21:44:12 +0200

    2026-09-14 01 - added testplan.ods
    TEST:[env:test-arduino-uno-blink]
    - passed

---
+++ src/libweitwapp/tests/misc/arduino/arduino-blink.test.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+/* 
+; Blinking on-board LED (13) on Arduino
+; PRECONDITIONS: none
+; TEST: the on-board LED blinks
+*/
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);   // Schalte die LED ein (HIGH ist die Spannung)
+  delay(1000);              // Warte 1 Sekunde (1000 Millisekunden)
+  digitalWrite(13, LOW);    // Schalte die LED aus
+  delay(1000);              // Warte 1 Sekunde
+}
